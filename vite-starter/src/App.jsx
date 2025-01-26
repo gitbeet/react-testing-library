0801ec06f904f0d0be88c75dcd538b31d58a68f7
gitbeet <nikig715@gmail.com>
2025-01-26 17:25:57 +0100

    checkbox exercise

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -3,17 +3,28 @@ import "./App.css";
 
 function App() {
   const [buttonClass, setButtonClass] = useState("red");
+  const [checked, setChecked] = useState(false);
   const toggleButtonClass = () => {
     setButtonClass((prev) => (prev === "red" ? "blue" : "red"));
   };
+  const toggleCheckbox = () => setChecked((prev) => !prev);
+
   return (
     <div>
       <button
+        disabled={checked}
         onClick={toggleButtonClass}
-        className={buttonClass}
+        className={checked ? "gray" : buttonClass}
       >
         Change to {buttonClass === "red" ? "blue" : "red"}
       </button>
+      <label htmlFor="checkbox">Check to disable button</label>
+      <input
+        id="checkbox"
+        onClick={toggleCheckbox}
+        type="checkbox"
+        checked={checked}
+      />
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -7,6 +7,7 @@ test("button starts with the correct label and color", () => {
   const buttonElement = screen.getByRole("button", { name: /blue/i });
   expect(buttonElement).toHaveClass("red");
 });
+
 test("button has the correct label and color after the click", () => {
   // render
   render(<App />);
@@ -18,4 +19,32 @@ test("button has the correct label and color after the click", () => {
   // check conditions
   expect(buttonElement).toHaveClass("blue");
   expect(buttonElement).toHaveTextContent("red");
+});
+
+test("checkbox flow", () => {
+  // render
+  render(<App />);
+  // find elements
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkBoxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  // check initial conditions
+  expect(buttonElement).toBeEnabled();
+  expect(checkBoxElement).not.toBeChecked();
+
+  // click the checkbox
+  fireEvent.click(checkBoxElement);
+  // check conditions after the checkbox click
+  expect(buttonElement).not.toBeEnabled();
+  expect(buttonElement).toHaveClass("gray");
+  expect(checkBoxElement).toBeChecked();
+
+  // click chekbox again to re-enable the button
+  fireEvent.click(checkBoxElement);
+  // check conditions after the second checkbox click
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("red");
+  expect(checkBoxElement).not.toBeChecked();
 });
