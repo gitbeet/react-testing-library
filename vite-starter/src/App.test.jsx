1a513a378cbe7a26fdc7fd0e40c720724a7c595a
gitbeet <nikig715@gmail.com>
2025-01-26 16:10:38 +0100

    first exercise

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
+test("button starts with the correct color", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  expect(buttonElement).toHaveClass("red");
 });
+// test("button starts with the correct initial text", () => {});
+// test("button has the correct color after the click", () => {});
+// test("button has the correct text after the click", () => {});
