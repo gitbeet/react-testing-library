8d68dc3faff92eb6b5b988894206f52901cb6dba
gitbeet <nikig715@gmail.com>
2025-01-27 20:47:14 +0100

    setting up msw

--- sundae-starter/src/setupTests.js
+++ sundae-starter/src/setupTests.js
@@ -1,1 +1,11 @@
 import "@testing-library/jest-dom";
+
+import { beforeAll, afterAll, afterEach } from "vitest";
+import { server } from "./mocks/server";
+
+// Establish API mocking (before all tests)
+beforeAll(() => server.listen());
+// Reset handlers between tests
+afterEach(() => server.resetHandlers());
+// Close the mock server after the tests are done
+afterAll(() => server.close());
