cd745698aa692fe8c752505854d71de5833c8019
gitbeet <nikig715@gmail.com>
2025-01-27 15:08:14 +0100

    sundaes on demand checkbox flow (bootstrap)

--- sundae-starter/src/SummaryForm.jsx
+++ sundae-starter/src/SummaryForm.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const SummaryForm = () => {
   const [checked, setChecked] = useState(false);
   const toggleChecked = () => setChecked((prev) => !prev);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+    </span>
+  );
+
   return (
     <div>
-      <div>
-        <label htmlFor="terms">I agree to Terms and Conditions</label>
-        <input
+      <Form.Group>
+        <Form.Check
           onChange={toggleChecked}
           checked={checked}
           type="checkbox"
-          id="terms"
+          label={checkboxLabel}
         />
-      </div>
-      <button disabled={!checked}>Confirm order</button>
+      </Form.Group>
+      <Button
+        variant="primary"
+        type="submit"
+        disabled={!checked}
+      >
+        Confirm order
+      </Button>
     </div>
   );
 };
